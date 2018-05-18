ff5bb2dfbb1fdfcb35cc6999ccef96e53a3891a7
Amy Funderburg <be-loved@sbcglobal.net>
2018-05-18 15:52:18 -0700

    Matching Functionality Added

--- js/main.js
+++ js/main.js
@@ -18,20 +18,19 @@ shells.sort(()=>0.5-Math.random());
 //make the game board
 let gameBoard = document.getElementById('game-board');
 
-//Function created with a lot of Mentor Support
-function makeCard (id, imageName){
+//Function created with Mentor Examples/Support
+function makeCard (imageName){
     const img = document.createElement('img');
     img.src = 'img/' + imageName;
     const div = document.createElement('div');
-    div.id = id;
     div.classList.add('game-card');
     div.appendChild(img);
     img.style.visibility = 'hidden';
     return div;
 }
 
-for (let i = 0; i < shells.length; i++) {
-    const card = makeCard(i, shells[i]);
+for (const shell of shells) {
+    const card = makeCard(shell);
     gameBoard.appendChild(card);
 }
 
@@ -40,7 +39,7 @@ let gameCard = document.getElementsByClassName('game-card');
 let cards = [...gameCard];
 for (const card of cards) {
     card.addEventListener('click', showCard);
-    card.addEventListener('click', addCard);
+    card.addEventListener('click', cardsOpen);
 }
 
 //Display clicked gameCard
@@ -50,19 +49,43 @@ function showCard () {
         this.firstChild.style.visibility = 'visible';
     }
 
-    this.classList.toggle("open");
-    this.classList.toggle("show");
+    this.classList.toggle('open');
+    this.classList.toggle('show');
+    this.classList.toggle('disabled');
 
 }
 
 //Add clicked gameCard to a *list* of "open" cards
 let openCards = [];
-function addCard() {
+function cardsOpen() {
     openCards.push(this);
     //test code
     console.log(...openCards);
+    if (openCards.length === 2) {
+       countMoves();
+        if (openCards[0].innerHTML === openCards[1].innerHTML){
+            openCards[0].classList.toggle('match');
+            openCards[1].classList.toggle('match');
+            openCards = [];
+        } else {
+            setTimeout(function() {
+                openCards[0].classList.remove('open', 'show', 'disabled');
+                openCards[1].classList.remove('open', 'show', 'disabled');
+                openCards[0].firstChild.style.visibility = 'hidden';
+                openCards[1].firstChild.style.visibility = 'hidden';
+                openCards = [];
+            }, 500);
+        }
+    }
 }
 
+//Count and show moves
+let moves = 0;
+let showMoves = document.querySelector('.moves');
+function countMoves(){
+    moves++;
+    showMoves.innerHTML = moves;
+}
 
 // /*
 //  * set up the event listener for a gameCard. If a gameCard is clicked:
